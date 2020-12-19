f4cc9970c6fcd65bc8c57bb33bfeb58e970033b7
benilyxdd <benilywork@icloud.com>
2020-12-19 15:09:45 +0800

    DEC 19 3:09PM Add cf 691 692

--- competition/codeforce/691(d2)/b.cpp
+++ competition/codeforce/691(d2)/b.cpp
@@ -4,18 +4,7 @@ using namespace std;
 #define ll long long
 
 void solve() {
-	int a, b, c;
-	cin >> a >> b >> c;
-	
-	int mn = min({a, b, c});
-	int total = a+b+c;
-	int times = total/9;
-	
-	if (total%3 == 0 && mn >= times && (total/3)%3 == 0) {
-		cout << "YES\n";
-	} else {
-		cout << "NO\n";
-	}
+
 }
 
 int main() {
@@ -23,7 +12,7 @@ int main() {
 	cin.tie(0);
 
 	int t = 1, i = 1;
-	cin >> t;
+	//cin >> t;
 	while(t--) {
 		//cout << "Case #" << i << ": ";
 		solve();

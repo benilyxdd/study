1aa1faedd70ba90f0279f1372ae59be4f92cff4b
benilyxdd <benilywork@icloud.com>
2020-12-30 17:33:35 +0800

    Add few cc contest

--- competition/codeforce/1450/d.cpp
+++ competition/codeforce/1450/d.cpp
@@ -3,27 +3,38 @@ using namespace std;
 
 #define ll long long
 const int mxN = (int)3e5+5;
-int n, ar[mxN];
-
-bool f(int x) {
-	for (int i = 1; i <= x; i++) {
-		if (ar[-1+i] != i || ar[n-i+1] != i)
-			return false;
-	}
-	return true;
-}
+int n, ar[mxN]; vector<int> cnt(mxN, 0);
+bool ans[mxN];
 
 void solve() {
-	//input
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> ar[i];
-	
-	//print
-	for (int i = 1; i < n; i++) {
-		cout << (f(i) ? 1 : 0);	
+	for (int i = 0; i < mxN; i++) {
+		ans[i] = 0;
+		cnt[i] = 0;
 	}
-	cout << (*min_element(ar, ar+n) == 1 ? 1 : 0) << "\n";
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> ar[i];
+		++cnt[ar[i]];
+	}
+	
+	int l = 0, r = n-1, x = 1;
+	for (; l < r && (ar[l] == x || ar[r] == x) && cnt[x] == 1; ++x) {
+		ans[r-l+1] = 1;
+
+		if (ar[l] == x)
+			l++;
+		else
+			r--;
+	}
+
+	if (l < r && cnt[x] > 0)
+		ans[r-l+1] = 1;
+	
+	ans[1] = *min_element(next(cnt.begin()), cnt.end());
+	for (int i = 1; i <= n; i++)
+		cout << ans[i];
+	cout << "\n";
 }
 
 int main() {

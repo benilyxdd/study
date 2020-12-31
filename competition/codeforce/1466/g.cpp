10e8d6df15777d61d3e5279736e346da87428a02
benilyxdd <benilywork@icloud.com>
2020-12-31 14:08:50 +0800

    DEC 31 2:08PM

--- competition/codeforce/1466/g.cpp
+++ competition/codeforce/1466/g.cpp
@@ -2,15 +2,8 @@
 using namespace std;
 
 #define ll long long
-const int MOD = (int)1e9+7, mxN = (int)5e5+5;
-string ar[mxN];
-int n;
 
 void solve() {
-	cin >> n;
-	for (int i = 0; i < n; i++) 
-		cin >> ar[i];
-	
 
 }
 
@@ -19,7 +12,7 @@ int main() {
 	cin.tie(0);
 
 	int t = 1, i = 1;
-	cin >> t;
+	//cin >> t;
 	while(t--) {
 		//cout << "Case #" << i << ": ";
 		solve();

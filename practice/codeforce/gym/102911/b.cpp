703b7aecaf54d2614c6fb6b14603779d8e4b838f
benilyxdd <benilywork@icloud.com>
2021-01-03 03:20:28 +0800

    JAN 3 3:20AM

--- practice/codeforce/gym/102911/b.cpp
+++ practice/codeforce/gym/102911/b.cpp
@@ -2,9 +2,24 @@
 using namespace std;
 
 #define ll long long
+const int mxN = 61;
+int ar[mxN], n, k;
+int cnt[mxN];
 
 void solve() {
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> ar[i];
+		cnt[ar[i]]++;
+	}
 
+	ll ans = 1;
+	for (int i = 0; i < mxN; i++) {
+		if (cnt[i]) {
+			ans *= 
+		}
+	}
+	cout << ans << "\n";
 }
 
 int main() {

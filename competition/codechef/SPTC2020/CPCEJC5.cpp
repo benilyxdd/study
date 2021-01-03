6a72e73d49f055585351016b4c6753769a31952d
benilyxdd <benilywork@icloud.com>
2021-01-03 16:55:52 +0800

    JAN 3 4:55PM Finished a dumb contest

---
+++ competition/codechef/SPTC2020/CPCEJC5.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve() {
+	int n;
+	cin >> n;
+	int x = n;
+	int sum = 0, i = 0;
+	vector<int> v;
+	while(n > 0) {
+		v.push_back(n%10);
+		n /= 10;
+		i++;
+	}
+	for (int& x : v) {
+		sum += pow(x, i);
+	}
+	cout << (sum == x ? "FEELS GOOD" : "FEELS BAD") << "\n";
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t = 1, i = 1;
+	cin >> t;
+	while(t--) {
+		//cout << "Case #" << i << ": ";
+		solve();
+		//i++;
+	}
+	return 0;
+}
+

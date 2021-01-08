1328ed2e799ee9eff5651da708fcba6fe09f79de
benilyxdd <benilyxdd@gmail.com>
2021-01-08 14:56:39 +0800

    JAN 8 2:56PM

--- competition/codechef/COOK125/CIRCHAOS.cpp
+++ competition/codechef/COOK125/CIRCHAOS.cpp
@@ -2,15 +2,42 @@
 using namespace std;
 
 #define ll long long
-const int mxM = (int)3e5+5;
-int ar[mxN], n, m;
+#define int ll
+
+int _gcd(int x, int y) {
+	if (x == 0)
+		return y;
+	return _gcd(y%x, x);
+}
+
+int mf(int gcd, int n) {
+	int mx = 0;
+	for (int i = 1; i*i <= gcd; i++) {
+		if (gcd%i == 0) {
+			if (i <= n)
+				mx = max(mx, i);
+			if (gcd/i <= n)
+				mx = max(mx, gcd/i);
+		}
+	}
+	return mx;
+}
 
 void solve() {
+	int n, m;
 	cin >> n >> m;
+	vector<int> ar(m);
 	for (int i = 0; i < m; i++)
 		cin >> ar[i];
 
+	int gcd = ar[0];
+	for (int i = 1; i < m; i++)
+		gcd = _gcd(gcd, ar[i]);
 
+	if (gcd > n) {
+		gcd = mf(gcd, n);
+	}
+	cout << n-gcd << '\n';
 }
 
 signed main() {
@@ -26,4 +53,5 @@ signed main() {
 	}
 	return 0;
 }
+
 

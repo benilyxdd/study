06d2b9280eab6bd50f822d1041f5733ea3378fe6
benilyxdd <benilywork@icloud.com>
2021-01-14 19:24:27 +0800

    JAN 14 7:24PM

---
+++ competition/google/hashcode/2021/practice/pizza.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int mxN = (int)1e5+5;
+int a, b, c, n;
+
+void solve() {
+
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t = 1, i = 1;
+	cin >> t;
+	while(t--) {
+		solve();
+	}
+	return 0;
+}
+
---
+++ practice/atcoder/abc172/c.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define int ll
+const int MAX = (int)1e18+9;
+const int mxN = (int)2e5+5;
+int n, m, k, a[mxN], b[mxN], dp[mxN];
+
+int f(int x, int y, int total) {
+	if (total > 
+}	
+
+void solve() {
+	cin >> n >> m >> k;	
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	for (int i = 0; i < m; i++) 
+		cin >> b[i];
+
+
+}
+
+signed main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t = 1, i = 1;
+	//cin >> t;
+	while(t--) {
+		//cout << "Case #" << i << ": ";
+		solve();
+		//i++;
+	}
+	return 0;
+}
+

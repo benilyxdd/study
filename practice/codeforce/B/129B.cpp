478049ac206b0b365d27b3b1a22062818db8bce5
benilyxdd <benilywork@icloud.com>
2021-02-10 03:54:15 +0800

    FEB 10 3:54AM

---
+++ practice/codeforce/B/129B.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int mxN = (int)1e5+5;
+int n, m;
+
+void solve() {
+	cin >> n >> m;
+
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t = 1, i = 1;
+	//cin >> t;
+	while(t--) {
+		//cout << "Case #" << i << ": ";
+		solve();
+		//i++;
+	}
+	return 0;
+}
+

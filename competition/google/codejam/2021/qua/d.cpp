7076015528dd6fc04eaaf5459075eb54f43f1b79
benilyxdd <benilywork@icloud.com>
2021-03-28 18:01:29 +0800

    MAR 28 6:01PM

--- competition/google/codejam/2021/qua/d.cpp
+++ competition/google/codejam/2021/qua/d.cpp
@@ -1,133 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	int t, n, q;
-	cin >> t >> n >> q;
-	for (int i = 0; i < t; i++) {
-		list<int> ls;
+int n, q;
 
-		int last_med = -1;
-		for (int j = 1; j <= n-2; j++) {
-			cout << j << ' ' << j+1 << ' ' << j+2 << '\n';
-			fflush(stdout);
+int ask(int a, int b, int c) {
+	cout << (a+1) << ' ' << (b+1) << ' ' << (c+1) << endl;
+	int res;
+	cin >> res;
+	if (res == -1) exit(0);
+	return (res-1);
+}
 
-			int med;
-			cin >> med;
+void solve() {
+	vector<int> ans = {0, 1};
+	for (int i = 2; i < n; i++) {
+		int low = 0, high = i;
+		bool done = false;
+		while(low + 1 < high) {
+			int mid = (low + high) >> 1;
+			int res = ask(ans[mid], ans[mid-1], i);
 
-			if (j == 1) {
-				for (int k = 1; k < 3; k++) {
-					if (k != med) {
-						ls.push_back(k);
-						break;
-					}
-				}
-				ls.push_back(med);
-				for (int k = 1; k <= 3; k++) {
-					if (k != med && k != ls.front()) {
-						ls.push_back(k);
-						break;
-					}
-				}
+			if (res == i) {
+				ans.insert(ans.begin() + mid, i);
+				done = true;
+				break;
+			}
+			
+			if (res == ans[mid-1]) {
+				high = mid-1;
 			} else {
-				if (med - last_med == 3) {
-					cout << last_med << ' ' << j+1 << ' ' << med << '\n';
-					fflush(stdout);
-
-					int new_med;
-					cin >> new_med;
-					
-					int pos = -1;
-					vector<int> v = {j+1, last_med};
-					sort(v.begin(), v.end());
-					
-					for (auto it = ls.begin(); it != ls.end(); it++) {
-						if (*it == v[0]) {
-							while(it != ls.end()) {
-								it++;
-								if (*it == v[1]) {
-									pos = 1;
-									break;
-								}
-							}
-							if (pos == -1) {
-								pos = 0;
-							}
-							it--;
-						}
-					}
-						
-					if (pos) {
-						auto it = find(ls.begin(), ls.end(), v[1]);
-						ls.insert(it, j+2);
-					} else {
-						auto it = find(ls.begin(), ls.end(), v[0]);
-						it++;
-						ls.insert(it, j+2);
+				low = mid+1;
+			}
+		}
+		if (!done) {
+			if (low < high) {
+				if (low < i - 1) {
+					int res = ask(ans[low], ans[low+1], i);
+					if (res == i) {
+						low++;
 					}
 				} else {
-					if (med == j+1) {
-						if (ls.back() == j+1) {
-							ls.push_back(j+2);
-						} else if (ls.front() == j) {
-							ls.push_front(j+2);
-						}
-						continue;
-					}
-					int pos = -1;
-					vector<int> v, v1;
-					for (int k = j; k <= j+2; k++) {
-						if (k != med) {
-							v1.push_back(k);
-						}
-					}
-
-					for (auto it = ls.begin(); it != ls.end(); it++) {
-						if (*it == v1[0] || *it == v1[1]) {
-							v.push_back(*it);
-						}
-					}
-
-					for (auto it = ls.begin(); it != ls.end(); it++) {
-						if (*it == v[0]) {
-							it++;
-							if (*it == v[1]) {
-								pos = 1;
-							} else {
-								pos = 0;
-							}
-							break;
-						}
-					}
-
-					if (pos) {
-						auto it = find(ls.begin(), ls.end(), v[1]);
-						ls.insert(it, j+2);
-					} else {
-						auto it = find(ls.begin(), ls.end(), v[0]);
-						it++;	
-						ls.insert(it, j+2);
+					int res = ask(ans[i-1], ans[i-2], i);
+					if (res != i) {
+						low++;
 					}
 				}
 			}
-			last_med = med;
-		}
-
-		assert((int)ls.size() == n);
-		//output
-		for (auto it = ls.begin(); it != ls.end(); it++) {
-			cout << *it << ' ';
-		}
-		cout << '\n';
-		fflush(stdout);
-
-		//output response
-		int ok;
-		cin >> ok;
-		if (ok == -1) {
-			return 0;
+			ans.insert(ans.begin() + low, i);
 		}
 	}
 
+	for (int i = 0; i < n; i++) {
+		cout << ans[i]+1 << ' ';
+	}
+	cout << endl;
+
+	int ok;
+	cin >> ok;
+	if (ok == -1) {
+		exit(0);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int t;
+	cin >> t >> n >> q;
+	while(t--) {
+		solve();
+	}
 	return 0;
 }
--- competition/google/codejam/2021/qua/e.cpp~
+++ competition/google/codejam/2021/qua/e.cpp~
@@ -8,32 +8,45 @@ void solve() {
 	for (int i = 0; i < 100; i++) {
 		cin >> ar[i];
 	}
-
-	int cnt[100];
+	
+	int cnt[10000];
+	int q[10000];
 	memset(cnt, 0, sizeof(cnt));
-	for (int i = 0; i < 100; i++) {
-		for (int j = 0; j < 10000; j++) {
-			if (ar[i][j] == '1') {
+	for (int i = 0; i < 10000; i++) {
+		q[i] = i+1;
+		for (int j = 0; j < 100; j++) {
+			if (ar[j][i] == '1') {
 				cnt[i]++;
 			}
 		}
 	}
 
-	int dd = 100000, ans = -1;
+	auto cmp = [&] (int& a, int& b) {
+		return (cnt[a-1] > cnt[b-1]);
+	};
+	sort(q, q+10000, cmp);
+	
+	vector<double> a(100, 0);
 	for (int i = 0; i < 100; i++) {
-		int ddd = abs(cnt[i]-5000);
-		if (ddd < dd) {
-			dd = ddd;
-			ans = i+1;
+		int k0 = 0, k1 = 0, ps = 0;
+		for (int j = 0; j < 10000; j++) {
+			if (ar[i][j] == '1') {
+				ps += k0;
+				k1++;
+			} else {
+				k0++;
+			}
 		}
+		a[i] = 1.0 * ps / (k0*k1);
 	}
-	cout << ans << '\n';
+
+	cout << max_element(a.begin(), a.end()) - a.begin() << '\n';
 }
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	//freopen("ein.txt", "r", stdin);
+	freopen("ein.txt", "r", stdin);
 	//freopen("eout.txt", "w", stdout);
 
 	int t = 1, i = 1, p;

9aaf5de2ae7187479f52a7a5583fa5b4b7657a4e
benilyxdd <benilywork@icloud.com>
2021-04-10 11:35:27 +0800

    APR 10 11:35AM

--- competition/google/codejam/2021/1a/a.cpp
+++ competition/google/codejam/2021/1a/a.cpp
@@ -2,12 +2,79 @@
 using namespace std;
 
 #define ll long long
+#define int ll
 
-void solve() {
-	
+int findnext(int& a, int b) {
+	int res = 0;
+	if (a > b) {
+		return res;
+	}
+
+	string a_str = to_string(a);
+	string b_str = to_string(b);
+
+	int a_len = a_str.length(); //2
+	int b_len = b_str.length(); //1
+	// cout << a_str << ' ' << b_str << '\n';
+
+	bool ok = 1;
+	for (int i = 0; i < a_len; i++) {
+		if (a_str[i] != b_str[i]) {
+			ok = 0;
+			break;
+		}
+	}
+
+	if (a_len == b_len) {
+		a *= 10;
+		return 1;
+	}
+
+	// cout << b_len << ' ' << a_len << '\n';
+	bool ok2 = false;
+	for (int i = b_len-1; i >= a_len; i--) {
+		// cout << b_str[i] << ' ';
+		if (b_str[i] != '9') {
+			ok2 = true;
+			break;
+		}
+	}
+	// cout << ok2 << '\n';
+
+	if (ok && ok2) {
+		a = b + 1;
+		res = b_len - a_len;
+	} else {
+		int t = b_len - a_len;
+		a *= pow(10, t);
+		if (a <= b) {
+			a *= 10;
+			t++;
+		}
+		res = t;
+	}
+
+	// cout << "res: " << res << '\n';
+	return res;
 }
 
-int main() {
+void solve() {
+	int n;
+	cin >> n;
+	vector<int> ar(n);
+	for (int& x : ar) {
+		cin >> x;
+	}
+
+	int ans = 0;
+	for (int i = 1; i < n; i++) {
+		int x = findnext(ar[i], ar[i - 1]);
+		ans += x;
+	}
+	cout << ans << '\n';
+}
+
+signed main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
--- competition/google/codejam/2021/1a/b.cpp
+++ competition/google/codejam/2021/1a/b.cpp
@@ -2,12 +2,66 @@
 using namespace std;
 
 #define ll long long
+#define int ll
 
-void solve() {
-	
+map<int, int> primeFact(int n) {
+	map<int, int> res;
+	while (n % 2 == 0) {
+		res[2]++;
+		n /= 2;
+	}
+
+	for (int i = 3; i <= sqrt(n); i += 2) {
+		while (n % i == 0) {
+			res[i]++;
+			n /= i;
+		}
+	}
+
+	if (n > 2) {
+		res[n]++;
+	}
+
+	return res;
 }
 
-int main() {
+void solve() {
+	int n;
+	cin >> n;
+	
+	map<int, int> m;
+	int sum = 0;
+
+	for (int i = 0; i < n; i++) {
+		int a, b;
+		cin >> a >> b;
+
+		m[a] = b;
+		sum += a * b;
+	}
+
+	for (int i = 1; i < sum; i++) {
+		int left = sum - i;
+		map<int, int> temp = primeFact(left);
+		bool ok = true;
+		int msum = 0;
+		for (pair<const int, int> p : temp) {
+			msum += p.first * p.second;
+			if (m[p.first] < p.second) {
+				ok = false;
+				break;
+			}
+		}
+		// cout << i << ": sum = " << msum << ' ' << "ok: " << ok << '\n';
+		if (ok && msum == i) {
+			cout << left << '\n';
+			return;
+		}
+	}
+	cout << 0 << '\n';
+}
+
+signed main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 

44ceb1a86ca2e3ce156a4dd91822f9a287cd4ea9
benilyxdd <benilywork@icloud.com>
2021-04-10 19:11:42 +0800

    APR 10 7:11PM

--- competition/google/codejam/2021/1a/a_p.cpp
+++ competition/google/codejam/2021/1a/a_p.cpp
@@ -72,8 +72,8 @@ void solve() {
 signed main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
+	freopen("ts2_input.txt", "r", stdin);
+	freopen("ts2_my_output.txt", "w", stdout);
 
 	int t = 1, i = 1;
 	cin >> t;

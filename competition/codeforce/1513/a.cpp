a726105b567c8417b3dcd906833fca2f3ca4776c
benilyxdd <benilywork@icloud.com>
2021-04-11 23:27:18 +0800

    APR 11 11:27PM

--- competition/codeforce/1513/a.cpp
+++ competition/codeforce/1513/a.cpp
@@ -4,7 +4,28 @@ using namespace std;
 #define ll long long
 
 void solve() {
+	int n, k;
+	cin >> n >> k;
 
+	int all = (n - 2 + 2 - 1) / 2;
+	if (k > all) {
+		cout << -1 << '\n';
+		return;
+	}
+
+	vector<int> ar(n);
+	for (int i = 0; i < n; i++) {
+		ar[i] = i + 1;
+	}
+
+	for (int i = 1, j = 0; i < n-1 && j < k; i += 2, j++) {
+		swap(ar[i], ar[i + 1]);
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << ar[i] << ' ';
+	}
+	cout << '\n';
 }
 
 int main() {
@@ -12,7 +33,7 @@ int main() {
 	cin.tie(0);
 
 	int t = 1, i = 1;
-	//cin >> t;
+	cin >> t;
 	while(t--) {
 		//cout << "Case #" << i << ": ";
 		solve();
--- competition/codeforce/1513/b.cpp
+++ competition/codeforce/1513/b.cpp
@@ -1,18 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define ll long long
-
-void solve() {
-
+#define int ll
+const int MOD = (int)1e9 + 7;
+ 
+ll fact(int n) {
+	ll res = 1LL;
+	for (int i = 2LL; i <= n; i++) {
+		res = (ll)(res * i) % MOD;
+	}
+	return res;
 }
-
-int main() {
+ 
+ll f(int cnt, int left) {
+	ll res = (cnt * (cnt - 1LL)) % MOD;
+	res = (ll)(res * fact(left)) % MOD;
+	return res;
+}
+ 
+void solve() {
+	int n;
+	cin >> n;
+	vector<int> ar(n);
+	map<int, int> cnt;
+	for (int& x : ar) {
+		cin >> x;
+		cnt[x]++;
+	}
+ 
+	ll ans = 0;
+	for (pair<const int, int>& p : cnt) {
+		// cout << p.first << ' ' << p.second << '\n';
+		ans = (ll)(ans + f(p.second, n - 2LL)) % MOD;
+	}
+	cout << (ans % MOD) << '\n';
+}
+ 
+signed main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-
+ 
 	int t = 1, i = 1;
-	//cin >> t;
+	cin >> t;
 	while(t--) {
 		//cout << "Case #" << i << ": ";
 		solve();
@@ -20,4 +50,3 @@ int main() {
 	}
 	return 0;
 }
-

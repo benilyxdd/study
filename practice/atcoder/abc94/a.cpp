f6b8f3990c214fd52554e88bf13cd93ff1c73616
benilyxdd <benilywork@icloud.com>
2021-05-07 04:59:12 +0800

    MAY 7 4:59AM

---
+++ practice/atcoder/abc94/a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	if (a > c) {
+		cout << "NO\n";
+		return;
+	}
+	c -= a;
+	if (c <= b) {
+		cout << "YES\n";
+	} else {
+		cout << "NO\n";
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int t = 1, i = 1;
+	//cin >> t;
+	while(t--) {
+		//cout << "Case #" << i << ": ";
+		solve();
+		//i++;
+	}
+	return 0;
+}
+

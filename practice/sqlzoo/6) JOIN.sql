9460b8c0a761eead56c9939414ec4dbea84290c0
benilyxdd <benilywork@icloud.com>
2021-05-13 02:07:00 +0800

    MAY 13 2:06AM

--- practice/sqlzoo/6) JOIN.sql
+++ practice/sqlzoo/6) JOIN.sql
@@ -26,3 +26,45 @@ SELECT player, teamid, coach, gtime
 -- 6
 SELECT mdate, teamname FROM game JOIN eteam ON (team1 = eteam.id)
  WHERE eteam.coach = 'Fernando Santos'
+
+-- 7
+SELECT player FROM goal JOIN game ON(id = matchid) 
+ WHERE game.stadium = 'National Stadium, Warsaw'
+
+-- 8
+SELECT DISTINCT player
+  FROM game JOIN goal ON matchid = id 
+    WHERE (team1='GER' OR team2='GER') AND teamid != 'GER'
+
+-- 9
+SELECT teamname, COUNT(gtime)
+  FROM eteam JOIN goal ON id=teamid
+GROUP BY teamname
+ ORDER BY teamname
+
+-- 10
+SELECT stadium, COUNT(gtime) 
+ FROM game JOIN goal ON (id = matchid)
+GROUP BY stadium
+
+-- 11
+SELECT matchid, mdate, COUNT(*)
+  FROM game JOIN goal ON matchid = id 
+ WHERE (team1 = 'POL' OR team2 = 'POL')
+GROUP BY matchid, mdate
+
+-- 12
+SELECT matchid, mdate, COUNT(gtime)
+ FROM goal JOIN game ON (matchid = id)
+WHERE goal.teamid = 'GER'
+ GROUP BY matchid, mdate
+
+-- 13
+SELECT mdate, 
+	   team1, 
+	   SUM(CASE WHEN teamid = team1 THEN 1 ELSE 0 END) AS score1, 
+	   team2, 
+	   SUM(CASE WHEN teamid = team2 THEN 1 ELSE 0 END) AS score2 FROM
+	game LEFT JOIN goal ON (id = matchid)
+	GROUP BY id
+	ORDER BY mdate, matchid, team1, team2

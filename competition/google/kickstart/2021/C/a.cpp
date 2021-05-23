571d806cb777c0dea89c1d3c54aa4cbdb468e256
benilyxdd <benilywork@icloud.com>
2021-05-23 20:25:05 +0800

    MAY 23 8:25PM

--- competition/google/kickstart/2021/C/a.cpp
+++ competition/google/kickstart/2021/C/a.cpp
@@ -2,9 +2,40 @@
 using namespace std;
 
 #define ll long long
+const int MOD = (int)1e9 + 7;
+
+bool ispal(string s) {
+	int n = s.length();
+	for (int i = 0; i < n / 2; i++) {
+		if (s[i] != s[n - i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
 
 void solve() {
+	int n, k;
+	string s;
+	cin >> n >> k >> s;
+	vector<int> mx(n, 100);
+	for (int i = 0; i < n; i++) {
+		mx[i] = min(mx[i], s[i] - 'a' + 1);
+		mx[n - i - 1] = min(mx[n - i - 1], s[i] - 'a' + 1);
+	}
 
+	ll ans = 1;
+	if (n == 2) {
+		ans = min(mx[0], mx[1]);
+	} else if (n == 1) {
+		cout << mx[0] - 1 << '\n';
+		return;
+	} else {
+		for (int i = 0; i < (n + 2 - 1) / 2 - 1; i++) {
+			ans = (ll)ans * (max(1, (mx[i] - 1) * k + mx[i + 1] * (mx[i] - 1 > 0))) % MOD;;
+		}
+	}
+	cout << ans - ispal(s) << '\n';
 }
 
 int main() {
--- competition/google/kickstart/2021/C/b.cpp
+++ competition/google/kickstart/2021/C/b.cpp
@@ -3,8 +3,38 @@ using namespace std;
 
 #define ll long long
 
-void solve() {
+ll findk2(ll n, ll k) {
+	ll lo = 0, hi = n / k;
+	while (lo < hi) {
+		ll mid = lo + (hi - lo) / 2;
+		ll x = (ll)(k + k + mid - 1) * mid / 2;
+		if (x > n) {
+			hi = mid - 1;
+		} else {
+			lo = mid + 1;
+		}
+	}
+	return lo;
+}
 
+void solve() {
+	ll n;
+	cin >> n;
+
+	ll ans = 0;
+	for (int i = 1; i <= n; i++) {
+		ll k2 = findk2(n, i);
+		ll x = (ll)(2 * i + (k2 - 1)) * k2 / 2, y;
+		if (x > n) {
+			y = (ll)x - (i + k2 - 1);
+		} else {
+			y = (ll)x + (i + k2);
+		}
+		if (x == n || y == n) {
+			ans++;
+		}
+	}
+	cout << ans << '\n';
 }
 
 int main() {

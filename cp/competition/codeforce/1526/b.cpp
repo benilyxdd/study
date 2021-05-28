84b97987b39f08938134bd018f0f74ad4eef8f8f
benilyxdd <benilywork@icloud.com>
2021-05-28 22:38:55 +0800

    MAY 28 10:38PM

--- cp/competition/codeforce/1526/b.cpp
+++ cp/competition/codeforce/1526/b.cpp
@@ -2,9 +2,26 @@
 using namespace std;
 
 #define ll long long
+vector<int> tc = {111111111, 11111111, 1111111, 111111, 11111, 1111, 111, 11};
 
 void solve() {
-
+	int n;
+	cin >> n;
+	bool ok = false;
+	for (int &x : tc) {
+		if (n % x == 0) {
+			ok = true;
+			break;
+		}
+	}
+	while (n > 10) {
+		for (int &x : tc) {
+			if (x >= n) {
+				n -= x;
+			}
+		}
+	}
+	cout << (ok || n == 0 ? "YES" : "NO") << '\n';
 }
 
 int main() {
@@ -12,7 +29,7 @@ int main() {
 	cin.tie(0);
 
 	int t = 1, i = 1;
-	//cin >> t;
+	cin >> t;
 	while(t--) {
 		//cout << "Case #" << i << ": ";
 		solve();

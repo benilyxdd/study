76f4d141e7914dd48b22b7b842ea2946897dc2b7
benilyxdd <benilywork@icloud.com>
2021-06-08 14:21:50 +0800

    well.. looks good

--- cp/competition/codeforce/1447/d.cpp
+++ cp/competition/codeforce/1447/d.cpp
@@ -17,32 +17,20 @@ void solve() {
 	string a, b;
 	cin >> n >> m >> a >> b;
 
+	int ans = 0;
 	vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= m; j++) {
 			if (a[i - 1] == b[j - 1]) {
-				dp[i][j] = dp[i - 1][j - 1] + 1;
+				dp[i][j] = dp[i - 1][j - 1] + 2;
 			} else {
-				dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+				dp[i][j] = max(max(dp[i - 1][j], dp[i][j - 1]) - 1, 0);
 			}
+			ans = max(ans, dp[i][j]);
 		}
 	}
-	debug(dp);
-	string lcs;
-	{
-		int i = n, j = m;
-		while (i > 0 && j > 0) {
-			if (a[i - 1] == b[j - 1]) {
-				lcs += a[i - 1];
-				i--, j--;
-			} else if (dp[i - 1][j] > dp[i][j - 1]) {
-				i--;
-			} else {
-				j--;
-			}
-		}
-	}
-	
+	// debug(dp);
+	cout << ans << '\n';
 }
 
 int main() {

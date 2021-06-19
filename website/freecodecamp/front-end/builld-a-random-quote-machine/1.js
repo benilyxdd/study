1d47436d2af22d5db583c0d4c2b6c7f6a407bfc7
benilyxdd <benilyxdd@gmail.com>
2021-06-20 01:44:49 +0800

    add js file

---
+++ website/freecodecamp/front-end/builld-a-random-quote-machine/1.js
@@ -0,0 +1,4 @@
+const url =
+    "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+const data = fetch(url).then(data => { return data.json() });

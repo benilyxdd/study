e413ea7e0ca0db2d8a2a0693c3f60c3660b5da04
benilyxdd <benilywork@icloud.com>
2021-06-25 16:37:14 +0800

    lc 684

---
+++ cp/practice/leetcode/684.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/redundant-connection/
+
+// O(n ^ 2)
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> p(2000, 0);
+        for (int i = 0; i < 2000; i++) {
+            p[i] = i;
+        }
+        
+        vector<int> res;
+        for (auto v : edges) {
+            int n1 = v[0], n2 = v[1];
+            while (n1 != p[n1]) n1 = p[n1];
+            while (n2 != p[n2]) n2 = p[n2];
+            if (n1 == n2) res = v;
+            else p[n1] = n2;
+        }
+        return res;
+    }
+};

cd30783ccda5d8b8e283fc43bba575763b2d9d01
benilyxdd <benilywork@icloud.com>
2021-06-27 18:00:37 +0800

    cf 1216c & template update

--- cp/practice/codeforce/C/1216C.cpp
+++ cp/practice/codeforce/C/1216C.cpp
@@ -2,14 +2,9 @@
 using namespace std;
 
 #define ll long long
-// direction vectors 
-// {up, down, right, left, upper-left, upper-right, lower-left, lower-right}
-int dr[8] = {-1, 1, 0, 0, -1, -1, 1, 1};
-int dc[8] = {0, 0, 1, -1, -1, 1, -1, 1};
 
-struct Rect { // Rectangle
+struct Rect {
 	int x1, y1, x2, y2;
-	// constructor(s)
 	Rect() {
 		x1 = 0, y1 = 0, x2 = 0, y2 = 0;
 	};
@@ -18,24 +13,22 @@ struct Rect { // Rectangle
 		x1 = a, y1 = b, x2 = c, y2 = d;
 	};
 
-	// normal method(s)
 	ll area() {
 		return 1ll * abs(y2 - y1) * abs(x2 - x1);
 	};
 
-	// debug method(s)
 	void print() {
 		cout << x1 << ' ' << y1 << ' ' << x2 << ' ' << y2 << '\n';
 	};
 };
 
-ll intersectArea(Rect a, Rect b) { // intersect area of two Rect
+ll inter(Rect a, Rect b) {
 	int x = max(0, min(a.x2, b.x2) - max(a.x1, b.x1));
 	int y = max(0, min(a.y2, b.y2) - max(a.y1, b.y1));
 	return (ll)x * y;
 }
 
-Rect intersect(Rect a, Rect b) { // coordinate of intersect area (x1, y1, x2, y2)
+Rect intersect(Rect a, Rect b) {
 	int i = max(min(a.x1, a.x2), min(b.x1, b.x2));
 	int j = max(min(a.y1, a.y2), min(b.y1, b.y2));
 	int k = min(max(a.x1, a.x2), max(b.x1, b.x2));
@@ -48,7 +41,23 @@ Rect intersect(Rect a, Rect b) { // coordinate of intersect area (x1, y1, x2, y2
 }
 
 void solve() {
+	Rect a, b, c;
+	cin >> a.x1 >> a.y1 >> a.x2 >> a.y2;
+	cin >> b.x1 >> b.y1 >> b.x2 >> b.y2;
+	cin >> c.x1 >> c.y1 >> c.x2 >> c.y2;
 
+	Rect inter_a_b = intersect(a, b);
+	Rect inter_a_c = intersect(a, c);
+	Rect black_inter_in_white = intersect(inter_a_b, inter_a_c);
+
+	ll total_black_area_in_white = inter_a_b.area() + inter_a_c.area() - black_inter_in_white.area();
+	ll white_area = a.area();
+	
+	if (white_area - total_black_area_in_white) {
+		cout << "YES\n";
+	} else {
+		cout << "NO\n";
+	}
 }
 
 int main() {

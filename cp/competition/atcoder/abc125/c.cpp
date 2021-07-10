2828d6beaad85af284b190d8e8f2c89572f2a6d9
benilyxdd <benilywork@icloud.com>
2021-07-10 18:23:27 +0800

    atcoder abc125 c

--- cp/competition/atcoder/abc125/c.cpp
+++ cp/competition/atcoder/abc125/c.cpp
@@ -2,15 +2,30 @@
 using namespace std;
 
 #define ll long long
-const int mxN = (int)2e5+5;
-int ar[mxN], n;
 
 void solve() {
+	int n;
 	cin >> n;
-	for (int i = 0; i < n; i++)
-		cin >> ar[i];
+	vector<int> ar(n);
+	for (int &x : ar) {
+		cin >> x;
+	}
 
+	vector<int> pre(n), suf(n);
+	pre[0] = ar[0];
+	suf[n - 1] = ar[n - 1];
+	for (int i = 1; i < n; i++) {
+		pre[i] = __gcd(pre[i - 1], ar[i]);
+	}
+	for (int i = n - 2; i >= 0; i--) {
+		suf[i] = __gcd(suf[i + 1], ar[i]);
+	}
 
+	int ans = max(suf[1], pre[n - 2]);
+	for (int i = 1; i < n - 1; i++) {
+		ans = max(ans, __gcd(pre[i - 1], suf[i + 1]));
+	}
+	cout << ans << '\n';
 }
 
 int main() {

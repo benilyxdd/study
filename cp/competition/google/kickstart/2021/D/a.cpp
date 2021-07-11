a79f1da28430b75f0cbb791af7d50c2859453dc2
benilyxdd <benilyxdd@gmail.com>
2021-07-11 15:02:04 +0800

    google ks round d

--- cp/competition/google/kickstart/2021/D/a.cpp
+++ cp/competition/google/kickstart/2021/D/a.cpp
@@ -3,11 +3,40 @@ using namespace std;
 
 #define ll long long
 
-void solve() {
+bool check(vector<ll> a) {
+	if ((a[1] - a[0]) == (a[2] - a[1])) {
+		return true;
+	}
+	return false;
+}
 
+void solve() {
+	vector<ll> ar(8);
+	for (ll &x : ar) {
+		cin >> x;
+	}
+
+	int mx = 0;
+	for (int i = 0; i < 4; i++) {
+		int x = i, y = 7 - i;
+		ll mid = (ll)(ar[x] + ar[y]) / 2;
+		int cnt = 0;
+		for (int j = 0; j < 4; j++) {
+			int x2 = j, y2 = 7 - j;
+			cnt += check({ar[x2], mid, ar[y2]});
+		}
+		cnt += check({ar[0], ar[3], ar[5]});
+		cnt += check({ar[2], ar[4], ar[7]});
+		cnt += check({ar[0], ar[1], ar[2]});
+		cnt += check({ar[5], ar[6], ar[7]});
+		mx = max(cnt, mx);
+	}
+	cout << mx << '\n';
 }
 
 int main() {
+	// freopen("input1.txt", "r", stdin);
+	// freopen("output1.txt", "w", stdout);
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
--- cp/competition/google/kickstart/2021/D/b.cpp
+++ cp/competition/google/kickstart/2021/D/b.cpp
@@ -4,10 +4,31 @@ using namespace std;
 #define ll long long
 
 void solve() {
+	int n, c;
+	cin >> n >> c;
+	vector<array<ll, 2>> range(n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < 2; j++) {
+			cin >> range[i][j];
+		}
+	}
 
+	map<ll, int> mp;
+	for (int i = 0; i < n; i++) {
+		mp[range[i][0]]++;
+		mp[range[i][1]]--;
+	}
+	priority_queue<array<ll, 2>> pq;
+	for (pair<const ll, int> &p : mp) {
+		pq.push({p.second, p.first});
+	}
+
+	
 }
 
 int main() {
+	freopen("input2.txt", "r", stdin);
+	freopen("output2.txt", "w", stdout);
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 

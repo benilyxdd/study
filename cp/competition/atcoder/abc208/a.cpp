5d087d703e72cb28eaf2e067c924c714d5ac1922
benilyxdd <benilyxdd@gmail.com>
2021-07-12 01:06:31 +0800

    atcoder abc208 a

--- cp/competition/atcoder/abc208/a.cpp
+++ cp/competition/atcoder/abc208/a.cpp
@@ -4,7 +4,13 @@ using namespace std;
 #define ll long long
 
 void solve() {
-
+	int a, b;
+	cin >> a >> b;
+	if (a * 6 >= b && b >= a) {
+		cout << "Yes";
+	} else {
+		cout << "No";
+	}
 }
 
 int main() {

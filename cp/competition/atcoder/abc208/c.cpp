2725e68f2693570d65bf5cbdd15a1bcba74e2284
benilyxdd <benilyxdd@gmail.com>
2021-07-12 01:17:49 +0800

    atcoder abc208 c

--- cp/competition/atcoder/abc208/c.cpp
+++ cp/competition/atcoder/abc208/c.cpp
@@ -4,7 +4,24 @@ using namespace std;
 #define ll long long
 
 void solve() {
+	ll n, k;
+	cin >> n >> k;
+	vector<array<ll, 2>> ar(n);
+	for (int i = 0; i < n; i++) {
+		cin >> ar[i][0];
+		ar[i][1] = i;
+	}
+	sort(ar.begin(), ar.end());
 
+	ll each = k / n;
+	ll left = k % n;
+	vector<ll> ans(n, each);
+	for (int i = 0; i < left; i++) {
+		ans[ar[i][1]]++;
+	}
+	for (ll &x : ans) {
+		cout << x << '\n';
+	}
 }
 
 int main() {

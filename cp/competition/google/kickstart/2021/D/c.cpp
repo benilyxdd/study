b0bb0d1b5b5e8840070fdd87cd7dad39da30b1a6
benilyxdd <benilyxdd@gmail.com>
2021-07-12 16:57:48 +0800

    google kc 2021 round d q3 half-half

--- cp/competition/google/kickstart/2021/D/c.cpp
+++ cp/competition/google/kickstart/2021/D/c.cpp
@@ -6,15 +6,73 @@ using namespace std;
 void solve() {
 	int n, m;
 	cin >> n >> m;
+	map<ll, ll> mp;
 	vector<array<ll, 2>> question(n);
 	for (int i = 0; i < n; i++) {
 		cin >> question[i][0] >> question[i][1];
+		mp[question[i][0]] = question[i][1];
 	}
+
+	vector<ll> ans;
 	for (int i = 0; i < m; i++) {
 		ll student;
 		cin >> student;
-		
+
+		auto it = mp.lower_bound(student);
+		ll key = (*it).first;
+		ll value = (*it).second;
+		if (it == mp.begin()) {
+			ll get = (*it).first;
+			ans.push_back(get);
+			mp.erase(it);
+			if (get + 1 <= value) {
+				mp[get + 1] = value;
+			}
+		} else if (student >= key && student <= value) {
+			ans.push_back(student);
+			mp.erase(it);
+			if (key <= student - 1) {
+				mp[key] = student - 1;
+			}
+			if (student + 1 <= value) {
+				mp[student + 1] = value;
+			}
+		} else if (student > value) {
+			if (it == mp.end()) {
+				it--;
+				key = (*it).first;
+				value = (*it).second;
+				ans.push_back(value);
+				mp.erase(it);
+				if (key <= value - 1) {
+					mp[key] = value - 1;
+				}
+			} else {
+				auto it2 = it;
+				it2--;
+				ll key2 = (*it2).first;
+				ll value2 = (*it2).second;
+				
+				if (abs(value2 - student) < abs(key - student)) {
+					ans.push_back(value2);
+					mp.erase(it2);
+					if (key2 <= value2 - 1) {
+						mp[key2] = value2 - 1;
+					}
+				} else {
+					ans.push_back(key);
+					mp.erase(it);
+					if (key - 1 <= value) {
+						mp[key - 1] = value;
+					}
+				}
+			}
+		}
 	}
+	for (ll &x : ans) {
+		cout << x << ' ';
+	}
+	cout << '\n';
 }
 
 int main() {

170ec43f572cc44efe1360fbcd96ea85c0c9127e
benilyxdd <benilyxdd@gmail.com>
2021-07-12 15:26:48 +0800

    google kc 2021 round d q3 revise half

--- cp/competition/google/kickstart/2021/D/c.cpp
+++ cp/competition/google/kickstart/2021/D/c.cpp
@@ -4,10 +4,22 @@ using namespace std;
 #define ll long long
 
 void solve() {
-
+	int n, m;
+	cin >> n >> m;
+	vector<array<ll, 2>> question(n);
+	for (int i = 0; i < n; i++) {
+		cin >> question[i][0] >> question[i][1];
+	}
+	for (int i = 0; i < m; i++) {
+		ll student;
+		cin >> student;
+		
+	}
 }
 
 int main() {
+	freopen("input3.txt", "r", stdin);
+	freopen("output3.txt", "w", stdout);
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 

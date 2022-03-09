be4fb8ec6bac2fe759967bfc58d8b8fd6a2b9f83
benily <leekwokhinben@gmail.com>
2022-03-10 02:20:00 +0800

    lc

---
+++ cp/practice/leetcode/509.cpp
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/fibonacci-number/
+
+// O(n ^ 2)
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0 || n == 1) return n;
+        return fib(n - 2) + fib(n - 1);
+    }
+};

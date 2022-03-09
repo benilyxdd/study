b6a62ae16f7194a32b04c4705f5a97b2db71d510
benily <leekwokhinben@gmail.com>
2022-03-10 02:24:10 +0800

    lc

--- cp/practice/leetcode/509.cpp
+++ cp/practice/leetcode/509.cpp
@@ -1,5 +1,19 @@
 // https://leetcode.com/problems/fibonacci-number/
 
+// O(n)
+class Solution {
+public:
+    int fib(int n) {
+        vector<int> dp(40);
+        dp[0] = 0;
+        dp[1] = dp[2] = 1;
+        for (int i = 3; i < 40; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
+
 // O(n ^ 2)
 class Solution {
 public:

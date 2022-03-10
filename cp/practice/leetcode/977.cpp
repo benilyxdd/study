2a517894c948192d70a9272deb0f208618d46c90
benily <leekwokhinben@gmail.com>
2022-03-10 13:01:15 +0800

    lc 977

--- cp/practice/leetcode/977.cpp
+++ cp/practice/leetcode/977.cpp
@@ -1,5 +1,25 @@
 //https://leetcode.com/problems/squares-of-a-sorted-array/
 
+// O(n) - two pointers
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        int lo = 0, hi = n - 1;
+        for (int i = 0; i < n; i++) {
+            if (abs(nums[lo]) > abs(nums[hi])) {
+                ans[n - i - 1] = nums[lo] * nums[lo++];
+            } else {
+                ans[n - i - 1] = nums[hi] * nums[hi--];
+            }
+        }
+        
+        return ans;
+    }
+};
+
+
 // O(n log n) - loop + sort
 class Solution {
 public:

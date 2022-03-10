dd57173abe4ea72fb5b690153be4999a3b942a33
benily <leekwokhinben@gmail.com>
2022-03-10 16:47:50 +0800

    lc

---
+++ cp/practice/leetcode/70.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/climbing-stairs/
+
+// O(n) - dp
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
---
+++ cp/practice/leetcode/746.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/min-cost-climbing-stairs/
+
+// O(n) - dp
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> dp(n + 1);
+        dp[0] = dp[1] = 0;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[n];
+    }
+};

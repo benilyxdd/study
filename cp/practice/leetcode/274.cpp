62c7961a2598b8db42b439585bedeaef21ca8a56
benily <leekwokhinben@gmail.com>
2022-03-18 22:35:43 +0800

    lc

--- cp/practice/leetcode/274.cpp
+++ cp/practice/leetcode/274.cpp
@@ -1,5 +1,26 @@
 // https://leetcode.com/problems/h-index/
 
+// O(n) - bucket list
+class Solution {
+ public:
+  int hIndex(vector<int>& c) {
+    int n = c.size();
+    vector<int> ar(n + 1, 0);
+    for (int& x : c) {
+      ar[min(n, x)]++;
+    }
+
+    int cnt = 0;
+    for (int i = n; i >= 0; i--) {
+      cnt += ar[i];
+      if (cnt >= i) {
+        return i;
+      }
+    }
+    return 0;
+  }
+};
+
 // O(n log n) - priority queue
 class Solution {
  public:

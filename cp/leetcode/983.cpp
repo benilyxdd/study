80282258bc9e4eede910dd6b0e27d7ca08f748a1
benily <leekwokhinben@gmail.com>
2022-03-24 03:04:34 +0800

    lc

--- cp/leetcode/983.cpp
+++ cp/leetcode/983.cpp
@@ -1,5 +1,29 @@
 // https://leetcode.com/problems/minimum-cost-for-tickets/
 
+// O(n) - dp (iterative)
+class Solution {
+ public:
+  int mincostTickets(vector<int>& days, vector<int>& costs) {
+    int n = days.size();
+    vector<int> dp(366, 0);
+    vector<int> travel(366, false);
+    for (int& day : days) {
+      travel[day] = true;
+    }
+
+    for (int i = 1; i < 366; i++) {
+      if (!travel[i]) {
+        dp[i] = dp[i - 1];
+      } else {
+        dp[i] = min({dp[i - 1] + costs[0], dp[max(0, i - 7)] + costs[1],
+                     dp[max(0, i - 30)] + costs[2]});
+      }
+    }
+
+    return dp[365];
+  }
+};
+
 // O(n) - dp (recursive)
 class Solution {
  public:

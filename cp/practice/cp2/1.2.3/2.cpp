d7cb637e2dd2d7aa05c5a94503c52374a6bc28b6
benily <leekwokhinben@gmail.com>
2022-03-23 19:03:34 +0800

    clean up 3 - move all competitions one level up

--- cp/practice/cp2/1.2.3/2.cpp
+++
@@ -1,6 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main() {
-	int n, x; cin >> n >> x; vector<int> ar(n); for (int i = 0; i < n; i++) cin >> ar[i];
-	cout << (find(ar.begin(), ar.end(), x) != ar.end() ? 1 : 0);
-}

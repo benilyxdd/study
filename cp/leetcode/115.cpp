1752f943fb4c9ff1d84f728c0ca6b84d54a97925
benily <leekwokhinben@gmail.com>
2022-03-26 23:47:08 +0800

    lc

--- cp/leetcode/115.cpp
+++ cp/leetcode/115.cpp
@@ -7,22 +7,19 @@ class Solution {
     int n = s.length();
     int m = t.length();
 
-    vector<vector<unsigned long long>> dp(m + 1,
-                                          vector<unsigned long long>(n + 1, 0));
+    vector<vector<unsigned long long>> dp(n + 1,
+                                          vector<unsigned long long>(m + 1, 0));
     for (int i = 0; i <= n; i++) {
-      dp[0][i] = 1;
+      dp[i][0] = 1;
     }
 
     for (int i = 1; i <= n; i++) {
       for (int j = 1; j <= m; j++) {
-        if (s[i - 1] == t[j - 1]) {
-          dp[j][i] = (unsigned long long)dp[j - 1][i - 1] + dp[j][i - 1];
-        } else {
-          dp[j][i] = dp[j][i - 1];
-        }
+        dp[i][j] = (unsigned long long)dp[i - 1][j] +
+                   (s[i - 1] == t[j - 1] ? dp[i - 1][j - 1] : 0);
       }
     }
 
-    return dp[m][n];
+    return dp[n][m];
   }
 };

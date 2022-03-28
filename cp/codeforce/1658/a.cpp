45dcf5ce0eb8f2a3f81de1f0f9b07628f4817eae
benily <leekwokhinben@gmail.com>
2022-03-28 20:48:06 +0800

    cf 1658

--- cp/codeforce/1658/a.cpp
+++ cp/codeforce/1658/a.cpp
@@ -4,20 +4,32 @@ using namespace std;
 #define ll long long
 
 void solve() {
+  int n;
+  string s;
+  cin >> n >> s;
 
+  int ans = 0;
+  for (int i = 0; i < n - 1; i++) {
+    if (s[i] == '0' && s[i + 1] == '0') {
+      ans += 2;
+    }
+    if (i + 2 < n && s[i] == '0' && s[i + 2] == '0' && s[i + 1] == '1') {
+      ans++;
+    }
+  }
+  cout << ans << '\n';
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-	int t = 1, i = 1;
-	//cin >> t;
-	while(t--) {
-		//cout << "Case #" << i << ": ";
-		solve();
-		//i++;
-	}
-	return 0;
+  int t = 1, i = 1;
+  cin >> t;
+  while (t--) {
+    // cout << "Case #" << i << ": ";
+    solve();
+    // i++;
+  }
+  return 0;
 }
-
--- cp/codeforce/1658/b.cpp
+++ cp/codeforce/1658/b.cpp
@@ -2,22 +2,23 @@
 using namespace std;
 
 #define ll long long
+const int MOD = 998244353;
 
 void solve() {
-
+  int n;
+  cin >> n;
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-	int t = 1, i = 1;
-	//cin >> t;
-	while(t--) {
-		//cout << "Case #" << i << ": ";
-		solve();
-		//i++;
-	}
-	return 0;
+  int t = 1, i = 1;
+  cin >> t;
+  while (t--) {
+    // cout << "Case #" << i << ": ";
+    solve();
+    // i++;
+  }
+  return 0;
 }
-

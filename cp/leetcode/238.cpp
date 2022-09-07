c1f5b185a59fc66fe81bb54740017d65d527b42e
Lee Kwok Hin <leekwokhinben@gmail.com>
2022-09-08 00:34:46 +0800

    lc

--- cp/leetcode/238.cpp
+++ cp/leetcode/238.cpp
@@ -1,6 +1,34 @@
 // https://leetcode.com/problems/product-of-array-except-self/
 
-// O(n) - loop
+// O(n) - another loop with prefix and suffix
+class Solution {
+ public:
+  vector<int> productExceptSelf(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> prefix(n), suffix(n);
+
+    prefix[0] = nums[0];
+    for (int i = 1; i < n; i++) {
+      prefix[i] = prefix[i - 1] * nums[i];
+    }
+
+    suffix[n - 1] = nums[n - 1];
+    for (int i = n - 2; i >= 0; i--) {
+      suffix[i] = suffix[i + 1] * nums[i];
+    }
+
+    vector<int> res(n);
+    res[0] = suffix[1];
+    res[n - 1] = prefix[n - 2];
+    for (int i = 1; i < n - 1; i++) {
+      res[i] = prefix[i - 1] * suffix[i + 1];
+    }
+
+    return res;
+  }
+};
+
+// O(n) - loop with prefix only
 class Solution {
  public:
   vector<int> productExceptSelf(vector<int>& nums) {
--- cp/leetcode/347.cpp
+++ cp/leetcode/347.cpp
@@ -1,5 +1,32 @@
 // https://leetcode.com/problems/top-k-frequent-elements/
 
+// O(n) - some hash map trick
+class Solution {
+ public:
+  vector<int> topKFrequent(vector<int> &nums, int k) {
+    int n = nums.size();
+    map<int, int> count;
+    for (int &x : nums) {
+      count[x]++;
+    }
+
+    vector<vector<int>> mp(n + 1, vector<int>());
+    for (pair<const int, int> &p : count) {
+      mp[p.second].push_back(p.first);
+    }
+
+    vector<int> res;
+    for (int i = n; i >= 0; i--) {
+      for (int &x : mp[i]) {
+        res.push_back(x);
+        if (res.size() == k) return res;
+      }
+    }
+
+    return res;
+  }
+};
+
 // O(n log n)
 class Solution {
  public:

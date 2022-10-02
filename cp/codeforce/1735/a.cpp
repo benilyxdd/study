bd93ff123910a04bf8883399b3987fcfad4fc2d6
benilyxdd <benilyxdd@gmail.com>
2022-10-02 23:45:29 +0800

    cf 1735 ac | wa | ? | ? | ? | ?

--- cp/codeforce/1735/a.cpp
+++ cp/codeforce/1735/a.cpp
@@ -4,7 +4,19 @@ using namespace std;
 #define ll long long
 
 void solve() {
+	int n;
+	cin >> n;
+	
+	int x = n - 4;
+	int y = x / 3;
+	int z = y * 2;
 
+	if (x % 3 == 2) {
+		y++;
+		z++;
+	}
+
+	cout << min(abs(1 - y), abs(y - z)) << '\n';
 }
 
 int main() {
@@ -12,7 +24,7 @@ int main() {
 	cin.tie(0);
 
 	int t = 1, i = 1;
-	//cin >> t;
+	cin >> t;
 	while(t--) {
 		//cout << "Case #" << i << ": ";
 		solve();
--- cp/codeforce/1735/b.cpp
+++ cp/codeforce/1735/b.cpp
@@ -2,17 +2,34 @@
 using namespace std;
 
 #define ll long long
+#define int ll
 
 void solve() {
+	int n;
+	cin >> n;
+	vector<int> ar(n);
+	for (int i = 0; i < n; i++) {
+		cin >> ar[i];
+	}
 
+	int cnt = 0;
+	int mn = ar[0];
+	int mx = max(1LL ,ar[0] * 2 - 2);
+	for (int i = 1; i < n; i++) {
+		int a = (ar[i] + mn - 1LL) / mn;
+		int b = (ar[i] + mx - 1LL) / mx;
+		cnt += min(a, b) - 1LL;
+	}
+
+	cout << cnt << '\n';
 }
 
-int main() {
+signed main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
 	int t = 1, i = 1;
-	//cin >> t;
+	cin >> t;
 	while(t--) {
 		//cout << "Case #" << i << ": ";
 		solve();
